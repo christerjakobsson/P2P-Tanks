24c4bd10b2c2f6332194b1722fc65ce45071d6f0
christerjakobsson <jakobsson.christer@gmail.com>
2015-04-14 11:07:20 +0200

    JEJ

--- webrtc/js/p2p_comm.js
+++ webrtc/js/p2p_comm.js
@@ -58,34 +58,16 @@ P2PCommClass.prototype.getPeerId = function() {
 P2PCommClass.prototype.createPeer = function(successFn, errorFn) {
    this._peer = new Peer({
        config: {
+           iceTransports: 'all',
            'iceServers': [
 
-                    {url:'stun:stun01.sipphone.com'},
-                    {url:'stun:stun.ekiga.net'},
-                    {url:'stun:stun.fwdnet.net'},
-                    {url:'stun:stun.ideasip.com'},
-                    {url:'stun:stun.iptel.org'},
-                    {url:'stun:stun.rixtelecom.se'},
-                    {url:'stun:stun.schlund.de'},
-                    {url:'stun:stun.l.google.com:19302'},
-                    {url:'stun:stun1.l.google.com:19302'},
-                    {url:'stun:stun2.l.google.com:19302'},
-                    {url:'stun:stun3.l.google.com:19302'},
-                    {url:'stun:stun4.l.google.com:19302'},
-                    {url:'stun:stunserver.org'},
-                    {url:'stun:stun.softjoys.com'},
-                    {url:'stun:stun.voiparound.com'},
-                    {url:'stun:stun.voipbuster.com'},
-                    {url:'stun:stun.voipstunt.com'},
-                    {url:'stun:stun.voxgratia.org'},
-                    {url:'stun:stun.xten.com'},
-                    { url: 'turn:numb.viagenie.ca', credential: 'muazkh', username: 'webrtc@live.com' }
+                    { urls:'stun:shinowa.tk:10000' },
+                    { urls: 'turn:shinowa.tk:10000', credential: '1234', username: 'test' }
            ]
        },
        key: Conf.peerJsKey,
        debug: Conf.peerJsDebug
    });
-
 
         /*
         // create a peer
